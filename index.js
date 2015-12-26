379d323f2aaf6527b1a60a7a1984e4e45ea7e6fa
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2015-12-26 18:50:23 +0100

    initial

---
+++ index.js
@@ -0,0 +1,61 @@
+function Fullscream() {
+  'use strict';
+
+  var isFull = false;
+
+  var _request = function _request(target) {
+    if (!target) {
+      return;
+    }
+
+    if (target.requestFullscreen) {
+      target.requestFullscreen();
+    } else if (target.mozRequestFullScreen) {
+      target.mozRequestFullScreen();
+    } else if (target.webkitRequestFullScreen) {
+      target.webkitRequestFullScreen();
+    }
+  };
+
+  var _exit = function _exit() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  };
+
+  var toggle = function _toggle(target) {
+    if (isFull) {
+      _exit();
+    } else {
+      _request(target);
+    }
+  };
+
+  document.addEventListener('fullscreenchange', function _onFullscreenChange() {
+    isFull = !!document.fullScreen;
+  }, false);
+
+  document.addEventListener('mozfullscreenchange', function _onFullscreenChange() {
+    isFull = !!document.mozFullScreen;
+  }, false);
+
+  document.addEventListener('webkitfullscreenchange', function _onFullscreenChange() {
+    isFull = !!document.webkitIsFullScreen;
+  }, false);
+
+  document.addEventListener('msfullscreenchange', function _onFullscreenChange() {
+    isFull = !!document.msFullscreenElement;
+  }, false);
+
+  return {
+    toggle: toggle
+  };
+}
+
+module.exports = Fullscream;
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Fullscream
+> Yet another super simple fullscreen API wrapper.
+
+### Setup
+```sh
+npm install fullscream --save
+# or
+bower install fullscream
+```
+
+### Usage
+```js
+var fullscream = require('fullscream');
+
+var $target = document.getElementById('target');
+var $resize = document.getElementById('resize');
+
+$resize.addEventListener('click', function() {
+	fullscream.toggle($target);
+}, false);
+```
