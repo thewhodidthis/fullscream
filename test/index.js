a18187d5533476a6b8ffa6752d94041798c75bb1
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-17 12:37:22 +0200

    Cleanup package file, add test related scripts and deps

---
+++ test/index.js
@@ -0,0 +1,66 @@
+const test = require('tape');
+const Fullscream = require('../');
+
+// Add favicon
+const linkTag = document.createElement('link');
+
+linkTag.rel = 'shortcut icon';
+linkTag.href = 'data:;base64,iVBORw0KGgo=';
+
+document.head.appendChild(linkTag);
+
+// Add styles
+const styleTag = document.createElement('style');
+const css = document.createTextNode(`
+  html {
+    cursor: pointer;
+    background: #fff;
+  }
+
+  body, pre {
+    margin: 0;
+  }
+
+  pre {
+    padding: 0.5rem;
+    box-sizing: border-box;
+  }
+
+  :-webkit-full-screen {
+    width: 50vw;
+    height: 50vh;
+    background: #ff0;
+  }
+  :-moz-full-screen {
+    background: #ff0;
+  }
+  :-ms-fullscreen {
+    background: #ff0;
+  }
+  :fullscreen {
+    background: #ff0;
+  }
+`);
+
+styleTag.appendChild(css);
+
+document.head.appendChild(styleTag);
+
+test('will default', (t) => {
+  const fullscreen = Fullscream();
+
+  t.ok(fullscreen, 'act on document body if target missing');
+  t.end();
+});
+
+test('will switch on click', (t) => {
+  const fullscreen = Fullscream(document.querySelector('pre'));
+
+  document.addEventListener('click', () => {
+    fullscreen.toggle();
+
+    t.pass('switch');
+    t.end();
+  });
+});
+
