20d334604a64450ffd61796f332e3ba3ea6b17ff
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-10 00:33:25 +0200

    test: move one dir up, drop semicolons, use kpow

---
+++ test.js
@@ -0,0 +1,46 @@
+const test = require('tape')
+const kpow = require('kpow')
+const fullscream = require('./')
+
+// Add styles
+const whenFull = 'color: #fff; width: 50vw; height: 50vh; background: transparent;'
+const styleTag = document.createElement('style')
+const css = document.createTextNode(`
+  html {
+    cursor: pointer;
+  }
+
+  body, pre {
+    margin: 0;
+  }
+
+  pre {
+    padding: 0.5rem;
+    box-sizing: border-box;
+  }
+
+  ${'-webkit-full-screen,-moz-full-screen,-ms-fullscreen,fullscreen'
+    .split(',')
+    .map(a => `:${a} { ${whenFull} }`)
+    .join('\n  ')}
+`)
+
+styleTag.appendChild(css)
+
+document.head.appendChild(styleTag)
+
+kpow()
+
+test('will default', (t) => {
+  t.equals(typeof fullscream, 'function', 'is ready')
+  t.end()
+})
+
+test('will switch on click', (t) => {
+  document.addEventListener('click', () => {
+    const state = fullscream(document.querySelector('pre'))
+
+    t.ok(state, 'is full')
+    t.end()
+  }, false)
+})
