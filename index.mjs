a4bee547046175844aecfe88d6b8516374da926d
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-22 09:01:24 +0100

    module: cleanup, go .mjs

---
+++ index.mjs
@@ -0,0 +1,42 @@
+// # Fullscream
+// Just another fullscren api wrapper
+
+// Helps toggle fullscreen mode
+const fullscream = ((page) => {
+  // Helps decide which vendor prefixed method or property to use
+  const patch = (source = {}, ...keys) => keys.reduce((memo, k) => {
+    const next = source[k]
+
+    return next === undefined ? memo : next
+  }, () => {})
+
+  // Check current status
+  const state = () => patch(page,
+    'webkitFullscreenElement',
+    'mozFullScreenElement',
+    'msFullscreenElement',
+    'fullscreenElement'
+  ) !== null
+
+  // Drop out of
+  const leave = () => patch(page,
+    'webkitExitFullscreen',
+    'mozCancelFullScreen',
+    'msExitFullscreen',
+    'exitFullscreen'
+  ).call(page)
+
+  // Ask for
+  const enter = (node = page.body) => patch(node,
+    'webkitRequestFullScreen',
+    'mozRequestFullScreen',
+    'msRequestFullscreen',
+    'requestFullscreen'
+  ).call(node)
+
+  // Toggle
+  /* eslint no-sequences: 1 */
+  return node => ((state() ? leave() : enter(node)), state())
+})(document)
+
+export default fullscream
